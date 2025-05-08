be7d08b10592960cccd01c34d2cb247a91ea786a
AllaHrushyna <allamakota89@gmail.com>
2025-05-08 02:44:25 +0200

    zet up

---
+++ mijnJavaScript.js
@@ -0,0 +1,15 @@
+
+gsap.to(".beeld-sunflower", {
+    rotation: 360,
+    duration: 20,
+    repeat: -1,
+    ease: "none"
+})
+
+gsap.to(".beeld-sunflower", {
+    y: 550,
+    x: -70,
+    duration: 3,
+    scale: 2,
+    delay: 5
+})
